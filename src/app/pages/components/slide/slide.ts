90682e8d2e103b56aaef06eec9957d92d81c2c68
Phuong Anh <phamphuonganh5112002@gmail.com>
2025-08-04 14:50:18 +0700

    navigate detail product by cate

--- src/app/pages/components/slide/slide.ts
+++ src/app/pages/components/slide/slide.ts
@@ -1,11 +1,27 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
+import { ICategory } from '../../../interfaces/ICategory';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-slide',
   imports: [],
   templateUrl: './slide.html',
-  styleUrl: './slide.css'
+  styleUrl: './slide.css',
 })
 export class Slide {
+  http = inject(HttpClient);
+  router = inject(Router);
+  categories: ICategory[] = [];
+  urlApi = 'http://localhost:3000/categories';
 
+  ngOnInit() {
+    this.http.get(this.urlApi).subscribe({
+      next: (data: any) => (this.categories = data),
+      error: (err) => console.log(err),
+    });
+  }
+  goToCategory(id: number) {
+    this.router.navigate(['/category', id]);
+  }
 }
