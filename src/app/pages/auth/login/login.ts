e971f0eb9afc9c196884068065ec18442a62da0d
Phuong Anh <phamphuonganh5112002@gmail.com>
2025-08-04 11:20:35 +0700

    signin and signup

--- src/app/pages/auth/login/login.ts
+++ src/app/pages/auth/login/login.ts
@@ -1,11 +1,46 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login.html',
-  styleUrl: './login.css'
+  styleUrl: './login.css',
 })
 export class Login {
+  http = inject(HttpClient);
+  router = inject(Router);
 
+  loginForm = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [
+      Validators.required,
+      Validators.minLength(6),
+    ]),
+  });
+
+  OnLogin() {
+    if (this.loginForm.invalid) return;
+
+    const loginData = this.loginForm.value;
+
+    this.http.post('http://localhost:3000/login', loginData).subscribe({
+      next: (res: any) => {
+        localStorage.setItem('access_token', res.accessToken);
+        localStorage.setItem('user', JSON.stringify(res.user));
+        alert('Đăng nhập thành công!');
+        this.router.navigate(['/']);
+      },
+      error: (err) => {
+        alert('Đăng nhập thất bại: ' + err.message);
+      },
+    });
+  }
 }
--- src/app/pages/auth/register/register.ts
+++ src/app/pages/auth/register/register.ts
@@ -1,11 +1,46 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './register.html',
-  styleUrl: './register.css'
+  styleUrl: './register.css',
 })
 export class Register {
+  router = inject(Router);
+  http = inject(HttpClient);
 
+  registerForm = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [
+      Validators.required,
+      Validators.minLength(6),
+    ]),
+    name: new FormControl('', Validators.required),
+  });
+
+  OnSubmit() {
+    if (!this.registerForm.valid) return;
+
+    const userData = this.registerForm.value;
+
+    this.http.post('http://localhost:3000/register', userData).subscribe({
+      next: () => {
+        alert('Đăng ký thành công!');
+        this.registerForm.reset();
+        this.router.navigate(['/login']);
+      },
+      error: (err) => {
+        alert('Đăng ký thất bại: ' + err.message);
+      },
+    });
+  }
 }
